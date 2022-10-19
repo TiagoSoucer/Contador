ca2952d1c1d1734d506cd074377271a9fd30abdd
Tiago Souza <74255959+TiagoSoucer@users.noreply.github.com>
2022-10-19 19:38:57 -0300

    Add files via upload

---
+++ contador.js
@@ -0,0 +1,36 @@
+function startTimer(duration, display){
+    var timer = duration, minutes, seconds;
+    setInterval(function(){
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+        
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if(--timer < 0){
+            timer = duration;
+        }
+
+    }, 1000);
+
+    
+}
+window.onload = function(){
+    var duration = 60 * 5; //Converter para segundos
+        display = document.querySelector('#timer'); //Selecionando o timer
+
+    startTimer(duration, display); //Iniciando o display
+}
+
+
+
+
+
+
+
+
+
+
+
